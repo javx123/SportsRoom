ece2004a9258247e87f6d572c59d0d4d341de002
Daniel Grosman <danny.grosman@gmail.com>
2017-12-18 15:06:21 -0500

    fixed merge conflicts

--- SportsRoom/JoinedGameViewController.swift
+++ SportsRoom/JoinedGameViewController.swift
@@ -38,14 +38,5 @@ class JoinedGameViewController: UIViewController, UITableViewDelegate, UITableVi
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         return IndicatorInfo(title: "Joined Games")
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-
+    
 }
