2312eb693507e2eeecbe87ce0de3a744b793e253
Daniel Grosman <danny.grosman@gmail.com>
2017-12-18 13:44:48 -0500

    updated detail views with labels and buttons

--- SportsRoom/JoinedGameViewController.swift
+++ SportsRoom/JoinedGameViewController.swift
@@ -8,28 +8,28 @@
 
 import UIKit
 
-class JoinedGameViewController: UIViewController {
+class JoinedGameViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "JoinCell", for: indexPath)
+        cell.textLabel?.text = "TEST"
+        return cell
+    }
 
 }
