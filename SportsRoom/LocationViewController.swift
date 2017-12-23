2081f699f1e8b6a1d87f3b540ebc03ccfe440926
Daniel Grosman <danny.grosman@gmail.com>
2017-12-23 18:59:37 -0500

    trying to get direction working for detail view - not working yet

--- SportsRoom/LocationViewController.swift
+++ SportsRoom/LocationViewController.swift
@@ -22,7 +22,6 @@ class LocationViewController: UIViewController, UINavigationBarDelegate, MKMapVi
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMapView()
-        
     }
     
     func setupMapView() {
@@ -30,14 +29,59 @@ class LocationViewController: UIViewController, UINavigationBarDelegate, MKMapVi
         camera.centerCoordinate = CLLocationCoordinate2D(latitude: latitude!, longitude: longitude!)
         camera.altitude = 1000
         mapView.camera = camera
-        
         let currentLocationAnnotation = MapPin(latitude: latitude!, longitude: longitude!, title: address!)
         mapView.addAnnotation(currentLocationAnnotation)
     }
-    
 }
 
-
+//extension LocationViewController: HandleMapSearch {
+//
+//    func dropPinZoomIn(placemark:MKPlacemark) {
+//        // cache the pin
+//        currentLocationPin = placemark
+//        // clear existing pins
+//        mapView.removeAnnotations(mapView.annotations)
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = placemark.coordinate
+//        annotation.title = placemark.name
+//        if let city = placemark.locality,
+//            let state = placemark.administrativeArea {
+//            annotation.subtitle = "\(city) \(state)"
+//        }
+//        mapView.addAnnotation(annotation)
+//        let span = MKCoordinateSpanMake(0.05, 0.05)
+//        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+//        mapView.setRegion(region, animated: true)
+//    }
+//}
+//
+//extension LocationViewController {
+//func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?{
+//    if annotation is MKUserLocation {
+//        return nil
+//    }
+//    let reuseId = "pin"
+//    var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+//    pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+//    pinView?.pinTintColor = UIColor.red
+//    pinView?.canShowCallout = true
+//    let smallSquare = CGSize(width: 47, height: 47)
+//    let point = CGPoint(x: 0, y: 0)
+//    let button = UIButton(frame: CGRect(origin: point, size: smallSquare))
+//    button.setBackgroundImage(UIImage(named: "car700"), for: .normal)
+//    button.addTarget(self, action: #selector(getDirections), for: .touchUpInside)
+//    pinView?.leftCalloutAccessoryView = button
+//    return pinView
+//}
+//
+//@objc func getDirections(){
+//    if let selectPin = currentLocationPin {
+//        let mapItem = MKMapItem(placemark: selectPin)
+//        let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+//        mapItem.openInMaps(launchOptions: launchOptions)
+//    }
+//}
+//}
 
 
 
--- SportsRoom/MapPin.swift
+++ SportsRoom/MapPin.swift
@@ -13,7 +13,6 @@ class MapPin: NSObject, MKAnnotation {
     var coordinate: CLLocationCoordinate2D
     var title: String?
     
-    
     init(latitude: Double, longitude: Double, title: String) {
         coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         self.title = title
