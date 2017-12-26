8db784ef9ae567128d127c6223f92c7acd75a17f
javx123 <javier.xing127@gmail.com>
2017-12-26 02:18:07 -0500

    Implemented cleaning expired games, some slight bugs let, that will still need testing

--- SportsRoom/ViewController.swift
+++ SportsRoom/ViewController.swift
@@ -16,12 +16,15 @@ import MapKit
 class ViewController: ButtonBarPagerTabStripViewController, UISearchBarDelegate, CLLocationManagerDelegate {
     
     let locationManager: CLLocationManager = CLLocationManager()
+    let dateFormatter = DateFormatter()
     let searchBar: UISearchBar = UISearchBar()
     var pulledData: Dictionary<String,Any> = [:]
     var currentUser: User?
     var customLocation: CLLocation?
     var customAddress: String?
     var searchRadius: Int?
+    var joinedGames: [Game] = []
+    var hostedGames: [Game] = []
 
     var createButton = UIBarButtonItem()
     var profileButton = UIBarButtonItem()
@@ -42,6 +45,7 @@ class ViewController: ButtonBarPagerTabStripViewController, UISearchBarDelegate,
         observeFireBase()
         createCurrentUser()
         configureView()
+        setupDateFormatter()
         enableLocationServices()
     }
     
@@ -146,6 +150,127 @@ class ViewController: ButtonBarPagerTabStripViewController, UISearchBarDelegate,
         let ref = Database.database().reference().child("users").child(userID!)
         ref.observe(.value) { (snapshot) in
              self.currentUser = User(snapshot: snapshot)
+            self.getJoinedGames()
+            self.getHostedGames()
+//            self.deleteExpiredGames()
+            
+        }
+    }
+    
+    func setupDateFormatter() {
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateFormatter.dateFormat = "MMM d, yyyy 'at' h:mm a"
+    }
+    
+    func deleteExpiredGames() {
+        let userID = Auth.auth().currentUser?.uid
+
+        for game in joinedGames {
+            let gameDate = dateFormatter.date(from: game.date)
+            if gameDate! < Date() {
+                let gameKey = game.gameID
+                let refUser = Database.database().reference().child("users").child(userID!).child("joinedGames")
+                refUser.child(gameKey).removeValue()
+                let refGame = Database.database().reference().child("games").child(gameKey).child("joinedPlayers")
+                refGame.child(userID!).removeValue()
+            }
+        }
+        
+        for game in hostedGames {
+            let gameDate = dateFormatter.date(from: game.date)
+            if gameDate! < Date() {
+                let gameKey = game.gameID
+                let refGame = Database.database().reference().child("games").child(gameKey)
+                    refGame.removeValue()
+                let refUserHosted = Database.database().reference().child("users").child(userID!).child("hostedGames")
+                    refUserHosted.child(gameKey).removeValue()
+                
+                for id in game.joinedPlayersArray! {
+                    let ref = Database.database().reference().child("users").child(id).child("joinedGames")
+                            ref.child(gameKey).removeValue()
+                }
+            }
+        }
+        
+    }
+    
+//    func cancelGame () {
+//        let userID = Auth.auth().currentUser?.uid
+//        let gameKey = currentGame.gameID
+//        let refGame = Database.database().reference().child("games").child(gameKey)
+//        refGame.removeValue()
+//        let refUserHosted = Database.database().reference().child("users").child(userID!).child("hostedGames")
+//        refUserHosted.child(gameKey).removeValue()
+//
+//        for id in currentGame.joinedPlayersArray! {
+//            let ref = Database.database().reference().child("users").child(id).child("joinedGames")
+//            ref.child(gameKey).removeValue()
+//        }
+//    }
+    
+    
+    
+    func getJoinedGames () {
+        let userID = Auth.auth().currentUser?.uid
+        let ref = Database.database().reference().child("users").child(userID!).child("joinedGames")
+        
+        ref.observeSingleEvent(of: .value) {(snapshot) in
+            let value = snapshot.value as? [String:String] ?? [:]
+            let gamesArrayID = Array(value.keys)
+            for id in gamesArrayID {
+                let ref = Database.database().reference().child("games").child(id)
+                ref.observeSingleEvent(of: .value) { (snapshot) in
+                    let game = Game(snapshot: snapshot)
+                    self.joinedGames.append(game)
+                    
+                    let gameDate = self.dateFormatter.date(from: game.date)
+                    if gameDate! < Date() {
+                        let gameKey = game.gameID
+                        let refUser = Database.database().reference().child("users").child(userID!).child("joinedGames")
+                        refUser.child(gameKey).removeValue()
+                        let refGame = Database.database().reference().child("games").child(gameKey).child("joinedPlayers")
+                        refGame.child(userID!).removeValue()
+                    }
+                }
+            }
+        }
+    }
+    
+    func getHostedGames () {
+        let userID = Auth.auth().currentUser?.uid
+        let ref = Database.database().reference().child("users").child(userID!).child("hostedGames")
+        
+        ref.observeSingleEvent(of: .value) {(snapshot) in
+            let value = snapshot.value as? [String:String] ?? [:]
+            let gamesArrayID = Array(value.keys)
+            for id in gamesArrayID {
+                let ref = Database.database().reference().child("games").child(id)
+                ref.observeSingleEvent(of: .value) { (snapshot) in
+                    let game = Game(snapshot: snapshot)
+                    self.hostedGames.append(game)
+                    
+                    let gameDate = self.dateFormatter.date(from: game.date)
+                    if gameDate! < Date() {
+                        let userID = Auth.auth().currentUser?.uid
+                        let gameKey = game.gameID
+                        let refUser = Database.database().reference().child("users").child(userID!).child("hostedGames")
+                        refUser.child(gameKey).removeValue()
+                    }
+                    if gameDate! < Date() {
+                        let gameKey = game.gameID
+                        let refGame = Database.database().reference().child("games").child(gameKey)
+                        refGame.removeValue()
+                        let refUserHosted = Database.database().reference().child("users").child(userID!).child("hostedGames")
+                        refUserHosted.child(gameKey).removeValue()
+
+                        for id in game.joinedPlayersArray! {
+                            let ref = Database.database().reference().child("users").child(id).child("joinedGames")
+                            ref.child(gameKey).removeValue()
+                        }
+                    }
+                }
+            }
         }
     }
     
