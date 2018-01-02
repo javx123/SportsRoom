b536635c154c72bb67250829bf588fceaa4c6079
Daniel Grosman <danny.grosman@gmail.com>
2018-01-01 21:22:47 -0500

    user defaults for login and changed image

--- SportsRoom/LoginViewController.swift
+++ SportsRoom/LoginViewController.swift
@@ -10,21 +10,28 @@ import UIKit
 import FirebaseAuth
 import FirebaseDatabase
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailTxtField: UITextField!
     @IBOutlet weak var passwordTxtField: UITextField!
     
-    @IBOutlet weak var blueView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        emailTxtField.delegate = self
+        passwordTxtField.delegate = self
+        
+        let defaults = UserDefaults.standard
+        emailTxtField.text = defaults.string(forKey: "emailTextFieldContent")
+        passwordTxtField.text = defaults.string(forKey: "passTextFieldContent")
         
         emailTxtField.backgroundColor = UIColor.clear
         emailTxtField.layer.borderColor = UIColor.white.cgColor
         emailTxtField.layer.borderWidth = 1
         emailTxtField.layer.cornerRadius = 7
         emailTxtField.textColor = UIColor.white
-        let emailPaddingView = UIView(frame: CGRect(x:0,y:0,width:35,height:emailTxtField.frame.height))
+        let emailPaddingView = UIView(frame: CGRect(x:0,y:0,width:40,height:emailTxtField.frame.height))
         emailTxtField.leftViewMode = UITextFieldViewMode.always
         emailTxtField.leftView = emailPaddingView
         
@@ -33,11 +40,23 @@ class LoginViewController: UIViewController {
         passwordTxtField.layer.borderWidth = 1
         passwordTxtField.layer.cornerRadius = 7
         passwordTxtField.textColor = UIColor.white
-        let passPaddingView = UIView(frame: CGRect(x:0,y:0,width:35,height:passwordTxtField.frame.height))
+        let passPaddingView = UIView(frame: CGRect(x:0,y:0,width:40,height:passwordTxtField.frame.height))
         passwordTxtField.leftViewMode = UITextFieldViewMode.always
         passwordTxtField.leftView = passPaddingView
         
     }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        emailTxtField.resignFirstResponder()
+        passwordTxtField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func screenTapped(_ sender: Any) {
+        emailTxtField.resignFirstResponder()
+        passwordTxtField.resignFirstResponder()
+    }
+    
     
     @IBAction func loginPressed(_ sender: UIButton) {
         if emailTxtField.text == "" || passwordTxtField.text == "" {
@@ -48,6 +67,8 @@ class LoginViewController: UIViewController {
                     StaticFunctions.displayAlert(title: "Error", message: error!.localizedDescription, uiviewcontroller: self)
                 } else {
                     print("Login Success!")
+                    UserDefaults.standard.set(self.emailTxtField.text, forKey: "emailTextFieldContent")
+                    UserDefaults.standard.set(self.passwordTxtField.text, forKey: "passTextFieldContent")
                     self.performSegue(withIdentifier: "toMain", sender: self)
                 }
             }
